a1fbe3a4490cc979cfc18a01c9d8b957a302f345
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-20 17:29:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,27 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  return array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max = array[0]
+  
+  array.each do |num|
+    if num > max 
+      max = num 
+    end 
+  end 
+  
+  return max 
 end
 
 def find_min_value(array)
-  # Add your solution here
+    min = array[0]
+  
+  array.each do |num|
+    if num < min 
+      min = num 
+    end 
+  end 
+  
+  return min 
 end
